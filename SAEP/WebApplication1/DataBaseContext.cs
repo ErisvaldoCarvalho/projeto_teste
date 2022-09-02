26f1b59a0152798fbc44fd310af489ca6701b36d
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-09-01 23:52:16 -0300

    Inicio EntityFrameworkCore
    Testado até o comando Script-Migration

---
+++ SAEP/WebApplication1/DataBaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using DAL;
+
+namespace WebApplication1
+{
+    public class DataBaseContext: DbContext
+    {
+        public DbSet<Equipamento> Equipamento { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Conexao.StringDeConexao);
+        }
+    }
+}
